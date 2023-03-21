eae99b9332d286f767a6f00f5f7c0e80d61da3a1
rakshann03 <panjalavisthruth@gmail.com>
2023-03-21 10:04:30 +0530

    End Of Section 2

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
